fd467aa686483b6027a9ae4b9bde92f2eb0f23ca
magali.gillard <magali.gillard@ehb.be>
2012-04-13 14:46:09 +0200

    installer

--- Application/Discovery/php/install/discovery_installer.class.php
+++ Application/Discovery/php/install/discovery_installer.class.php
@@ -1,15 +1,12 @@
 <?php
 namespace application\discovery;
 
-use common\libraries\WebApplication;
 use common\libraries\Installer;
 
 /**
  * @author Hans De Bisschop
  * @package application.discovery
  */
-
-require_once WebApplication :: get_application_class_lib_path('discovery') . 'discovery_data_manager.class.php';
 
 class DiscoveryInstaller extends Installer
 {
@@ -22,9 +19,5 @@ class DiscoveryInstaller extends Installer
         parent :: __construct($values, DiscoveryDataManager :: get_instance());
     }
 
-    function get_path()
-    {
-        return parent :: get_path();
-    }
 }
 ?>
