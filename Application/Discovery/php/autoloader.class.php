541d806526b4f692056f45ce05fceef34fedca54
Hans De Bisschop <hans.de.bisschop@ehb.be>
2012-05-13 14:58:04 +0200

    Package activator + autoloader

--- Application/Discovery/php/autoloader.class.php
+++ Application/Discovery/php/autoloader.class.php
@@ -55,6 +55,7 @@ class Autoloader
          'ModuleInstanceForm' => '/lib/module_instance_manager/forms/module_instance_form.class.php',
          'DefaultModuleInstanceTableCellRenderer' => '/lib/module_instance_manager/module_instance_table/default_module_instance_table_cell_renderer.class.php',
          'DefaultModuleInstanceTableColumnModel' => '/lib/module_instance_manager/module_instance_table/default_module_instance_table_column_model.class.php',
+         'Activator' => '/package/activate/activator.class.php',
          'DiscoveryInstaller' => '/package/install/discovery_installer.class.php',
          'SettingsConnector' => '/settings/connector.class.php'
     );
---
+++ Application/Discovery/php/package/activate/activator.class.php
@@ -0,0 +1,8 @@
+<?php
+namespace application\discovery;
+
+class Activator extends \common\libraries\Activator
+{
+
+}
+?>
