91752902b26b605141fa5c2e400767b8591da36d
magali.gillard <magali.gillard@ehb.be>
2012-06-19 11:44:16 +0200

    eagle images

--- Application/Discovery/data_source/bamaflex/sql/chamilo_discovery_bamaflex_history.sql
+++ Application/Discovery/data_source/bamaflex/sql/chamilo_discovery_bamaflex_history.sql
@@ -1,5 +1,5 @@
-CREATE DATABASE  IF NOT EXISTS `chamilo` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci */;
-USE `chamilo`;
+CREATE DATABASE  IF NOT EXISTS `desiderius` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci */;
+USE `desiderius`;
 -- MySQL dump 10.13  Distrib 5.5.9, for Win32 (x86)
 --
 -- Host: localhost    Database: chamilo
