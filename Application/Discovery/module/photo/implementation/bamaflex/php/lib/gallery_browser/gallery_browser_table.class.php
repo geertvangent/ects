d882e11879c145b24c3cdaea13de512cbd58c7d3
Hans De Bisschop <hans.de.bisschop@ehb.be>
2012-06-19 19:05:00 +0200

    Show less photos by default

--- Application/Discovery/module/photo/implementation/bamaflex/php/lib/gallery_browser/gallery_browser_table.class.php
+++ Application/Discovery/module/photo/implementation/bamaflex/php/lib/gallery_browser/gallery_browser_table.class.php
@@ -16,10 +16,10 @@ class GalleryBrowserTable extends GalleryObjectTable
         $property_model = new GalleryBrowserTablePropertyModel();
         $cell_renderer = new GalleryBrowserTableCellRenderer($browser);
         $data_provider = new GalleryBrowserTableDataProvider($browser, $condition);
-                
+
         parent :: __construct($data_provider, self :: DEFAULT_NAME, $cell_renderer, $property_model);
-        
-        $this->set_default_row_count(4);
+
+        $this->set_default_row_count(2);
         $this->set_default_column_count(4);
         $this->set_additional_parameters($parameters);
     }
