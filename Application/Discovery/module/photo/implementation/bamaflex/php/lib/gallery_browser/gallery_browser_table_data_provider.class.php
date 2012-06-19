c4a03e2f5ec3f4cc3897cbcd34a7e7d41da86a31
Hans De Bisschop <hans.de.bisschop@ehb.be>
2012-06-19 17:15:19 +0200

    New methods to retrieve users.

--- Application/Discovery/module/photo/implementation/bamaflex/php/lib/gallery_browser/gallery_browser_table_data_provider.class.php
+++ Application/Discovery/module/photo/implementation/bamaflex/php/lib/gallery_browser/gallery_browser_table_data_provider.class.php
@@ -1,8 +1,9 @@
 <?php
 namespace application\discovery\module\photo\implementation\bamaflex;
 
+use common\libraries\DataClassCountParameters;
+use common\libraries\DataClassRetrievesParameters;
 use user\UserDataManager;
-
 use common\libraries\GalleryObjectTableDataProvider;
 
 class GalleryBrowserTableDataProvider extends GalleryObjectTableDataProvider
@@ -11,12 +12,14 @@ class GalleryBrowserTableDataProvider extends GalleryObjectTableDataProvider
     function get_objects($offset, $count, $order_property = null)
     {
         $order_property = $this->get_order_property($order_property);
-        return UserDataManager :: get_instance()->retrieve_users($this->get_condition(), $offset, $count, $order_property);
+        $parameters = new DataClassRetrievesParameters($this->get_condition(), $count, $offset, $order_property);
+        return \user\DataManager :: retrieves(\user\User :: class_name(), $parameters);
     }
 
     function get_object_count()
     {
-        return  UserDataManager :: get_instance()->count_users($this->get_condition());
+        $parameters = new DataClassCountParameters($this->get_condition());
+        return \user\DataManager :: count(\user\User :: class_name(), $parameters);
     }
 }
 ?>
