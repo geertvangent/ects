215236e396d9a4ed37c029df243e6f9726aac4de
magali.gillard <magali.gillard@ehb.be>
2012-09-28 14:20:39 +0200

    cron jobs for synchronization

---
+++ Application/Sync/bamaflex/php/cron/groups.php
@@ -0,0 +1,35 @@
+<?php
+namespace application\ehb_sync\bamaflex;
+
+/**
+ * This script will load the requested application and launch it.
+ */
+use common\libraries\Display;
+use Exception;
+
+require_once dirname(__FILE__) . '/../../../../../common/common.inc.php';
+
+try
+{
+    ini_set("memory_limit", "-1");
+    ini_set("max_execution_time", "18000");
+    echo '<pre>';
+    Synchronization :: log('Group sync started', date('c', time()));
+    
+    $root_group = \group\DataManager :: get_root_group();
+    
+    $synchronization = GroupSynchronization :: factory('academic_year', new DummyGroupSynchronization($root_group));
+    $synchronization->run();
+    
+    $synchronization = GroupSynchronization :: factory('central_administration', new DummyGroupSynchronization($root_group));
+    $synchronization->run();
+    
+    Synchronization :: log('Group sync ended', date('c', time()));
+    echo '</pre>';
+}
+catch (\Exception $exception)
+{
+    echo 'Synchronization failed';
+}
+
+?>
