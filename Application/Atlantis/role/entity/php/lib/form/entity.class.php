d3e82882af1ccb2ecfbcef14ff8b2da222ba56e3
Hans De Bisschop <hans.de.bisschop@ehb.be>
2013-05-22 12:08:27 +0200

    Rules added for entity start & end date

--- Application/Atlantis/role/entity/php/lib/form/entity.class.php
+++ Application/Atlantis/role/entity/php/lib/form/entity.class.php
@@ -24,7 +24,7 @@ class EntityForm extends FormValidator
     public function __construct($application, $action)
     {
         parent :: __construct('application', 'post', $action);
-        
+
         $this->application = $application;
         $this->build();
         $this->setDefaults();
@@ -34,66 +34,91 @@ class EntityForm extends FormValidator
     {
         // entity
         $user_entity = new NewUserEntity();
-        
+
         $entities = array();
         $entities[NewUserEntity :: ENTITY_TYPE] = $user_entity;
         $entities[NewPlatformGroupEntity :: ENTITY_TYPE] = new NewPlatformGroupEntity();
-        
+
         $types = new AdvancedElementFinderElementTypes();
-        
+
         foreach ($entities as $entity)
         {
             $types->add_element_type($entity->get_element_finder_type());
         }
-        
+
         $this->addElement('advanced_element_finder', self :: PROPERTY_ENTITY, Translation :: get('UserGroup'), $types);
-        
+
         // roles
         $types = new AdvancedElementFinderElementTypes();
-        
+
         $types->add_element_type(
-                new AdvancedElementFinderElementType('roles', Translation :: get('Roles'), 'application\atlantis\role', 
-                        'roles_feed', array()));
-        
+            new AdvancedElementFinderElementType(
+                'roles',
+                Translation :: get('Roles'),
+                'application\atlantis\role',
+                'roles_feed',
+                array()));
+
         $this->addElement('advanced_element_finder', self :: PROPERTY_ROLE, Translation :: get('Roles'), $types);
-        
+
         // context
         $types = new AdvancedElementFinderElementTypes();
-        
+
         $types->add_element_type(
-                new AdvancedElementFinderElementType('contexts', Translation :: get('Contexts'), 
-                        'application\atlantis\context', 'contexts_feed', array()));
-        
+            new AdvancedElementFinderElementType(
+                'contexts',
+                Translation :: get('Contexts'),
+                'application\atlantis\context',
+                'contexts_feed',
+                array()));
+
         $this->addElement('advanced_element_finder', self :: PROPERTY_CONTEXT, Translation :: get('Contexts'), $types);
-        
+
         // start date
-        $this->addElement('text', self :: PROPERTY_START_DATE, Translation :: get('StartDate'), 
-                'id="start_date" style="width:120px;"');
-        // $this->addGroup($start_date, Task :: PROPERTY_START_DATE, Translation :: get('StartDate'), '<br />', false);
-        // $this->get_renderer()->setGroupElementTemplate('{element}', Task :: PROPERTY_START_DATE);
-        
+        $this->addElement(
+            'text',
+            self :: PROPERTY_START_DATE,
+            Translation :: get('StartDate'),
+            'id="start_date" style="width:120px;"');
+        $this->addRule(
+            self :: PROPERTY_START_DATE,
+            Translation :: get('ThisFieldIsRequired', null, Utilities :: COMMON_LIBRARIES),
+            'required');
+
         // end date
-        $this->addElement('text', self :: PROPERTY_END_DATE, Translation :: get('EndDate'), 
-                'id="end_date" style="width:120px;"');
-        // $this->addGroup($end_date, Task :: PROPERTY_END_DATE, Translation :: get('EndDate'), '<br />', false);
-        // $this->get_renderer()->setGroupElementTemplate('{element}', Task :: PROPERTY_END_DATE);
-        
+        $this->addElement(
+            'text',
+            self :: PROPERTY_END_DATE,
+            Translation :: get('EndDate'),
+            'id="end_date" style="width:120px;"');
+        $this->addRule(
+            self :: PROPERTY_END_DATE,
+            Translation :: get('ThisFieldIsRequired', null, Utilities :: COMMON_LIBRARIES),
+            'required');
+
         // buttons
-        $buttons[] = $this->createElement('style_submit_button', 'submit', 
-                Translation :: get('Create', null, Utilities :: COMMON_LIBRARIES), array('class' => 'positive'));
-        $buttons[] = $this->createElement('style_reset_button', 'reset', 
-                Translation :: get('Reset', null, Utilities :: COMMON_LIBRARIES), array('class' => 'normal empty'));
-        
+        $buttons[] = $this->createElement(
+            'style_submit_button',
+            'submit',
+            Translation :: get('Create', null, Utilities :: COMMON_LIBRARIES),
+            array('class' => 'positive'));
+        $buttons[] = $this->createElement(
+            'style_reset_button',
+            'reset',
+            Translation :: get('Reset', null, Utilities :: COMMON_LIBRARIES),
+            array('class' => 'normal empty'));
+
         $this->addGroup($buttons, 'buttons', null, '&nbsp;', false);
-        
-        $this->addElement('html', 
-                ResourceManager :: get_instance()->get_resource_html(
-                        Path :: namespace_to_full_path(__NAMESPACE__, true) . 'resources/javascript/dates.js'));
+
+        $this->addElement(
+            'html',
+            ResourceManager :: get_instance()->get_resource_html(
+                Path :: namespace_to_full_path(__NAMESPACE__, true) . 'resources/javascript/dates.js'));
     }
 
     /**
      * Sets default values.
-     * 
+     *
      * @param $defaults array Default values for this form's parameters.
      */
     public function setDefaults($defaults = array ())
