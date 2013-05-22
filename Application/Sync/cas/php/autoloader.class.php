ed5eefc05d3fc9f0c1f80ce856ce051b5b449e97
magali.gillard <magali.gillard@ehb.be>
2013-05-22 10:38:59 +0200

    ...

--- Application/Sync/cas/php/autoloader.class.php
+++ Application/Sync/cas/php/autoloader.class.php
@@ -6,29 +6,30 @@ class Autoloader
 
     /**
      * The array mapping class names to paths
-     * 
+     *
      * @var multitype:string
      */
-    private static $map = array(
-        'Autoloader' => '/autoloader.class.php', 
-        'DataManager' => '/lib/data_manager.class.php', 
-        'DataManagerInterface' => '/lib/data_manager_interface.class.php', 
-        'DoctrineDataManager' => '/lib/data_manager/doctrine.class.php', 
-        'Mdb2DataManager' => '/lib/data_manager/mdb2.class.php', 
-        'Manager' => '/lib/manager/manager.class.php', 
-        'AllUsersComponent' => '/lib/manager/component/all_users.class.php', 
-        'BrowserComponent' => '/lib/manager/component/browser.class.php', 
-        'StatisticsComponent' => '/lib/manager/component/statistics.class.php', 
-        'Synchronization' => '/lib/synchronization/synchronization.class.php', 
-        'UserSynchronization' => '/lib/synchronization/type/user.class.php', 
-        'AllUserSynchronization' => '/lib/synchronization/type/user/all.class.php', 
-        'Activator' => '/package/activate/activator.class.php', 
-        'Deactivator' => '/package/deactivate/deactivator.class.php', 
-        'Installer' => '/package/install/installer.class.php');
+     private static $map = array(
+         'Autoloader' => '/autoloader.class.php',
+         'DataManager' => '/lib/data_manager.class.php',
+         'DataManagerInterface' => '/lib/data_manager_interface.class.php',
+         'DoctrineDataManager' => '/lib/data_manager/doctrine.class.php',
+         'Mdb2DataManager' => '/lib/data_manager/mdb2.class.php',
+         'Manager' => '/lib/manager/manager.class.php',
+         'AllUsersComponent' => '/lib/manager/component/all_users.class.php',
+         'BrowserComponent' => '/lib/manager/component/browser.class.php',
+         'StatisticsComponent' => '/lib/manager/component/statistics.class.php',
+         'Synchronization' => '/lib/synchronization/synchronization.class.php',
+         'UserSynchronization' => '/lib/synchronization/type/user.class.php',
+         'AllUserSynchronization' => '/lib/synchronization/type/user/all.class.php',
+         'Activator' => '/package/activate/activator.class.php',
+         'Deactivator' => '/package/deactivate/deactivator.class.php',
+         'Installer' => '/package/install/installer.class.php'
+    );
 
     /**
      * Try to load the class
-     * 
+     *
      * @param $classname string
      * @return boolean
      */
@@ -39,13 +40,13 @@ class Autoloader
             require_once __DIR__ . self :: $map[$classname];
             return true;
         }
-        
+
         return false;
     }
 
     /**
      * Synchronize the autoloader
-     * 
+     *
      * @param $update boolean
      * @return multitype:string
      */
@@ -53,4 +54,6 @@ class Autoloader
     {
         return \common\libraries\AutoloaderUtilities :: synch(__DIR__, __DIR__, $update);
     }
+
 }
+?>
--- Application/Sync/php/autoloader.class.php
+++ Application/Sync/php/autoloader.class.php
@@ -6,27 +6,28 @@ class Autoloader
 
     /**
      * The array mapping class names to paths
-     * 
+     *
      * @var multitype:string
      */
-    private static $map = array(
-        'Autoloader' => '/autoloader.class.php', 
-        'DataManager' => '/lib/data_manager.class.php', 
-        'DataManagerInterface' => '/lib/data_manager_interface.class.php', 
-        'DoctrineDataManager' => '/lib/data_manager/doctrine.class.php', 
-        'Mdb2DataManager' => '/lib/data_manager/mdb2.class.php', 
-        'Manager' => '/lib/manager/manager.class.php', 
-        'AtlantisComponent' => '/lib/manager/component/atlantis.class.php', 
-        'BamaflexComponent' => '/lib/manager/component/bamaflex.class.php', 
-        'BrowserComponent' => '/lib/manager/component/browser.class.php', 
-        'CasComponent' => '/lib/manager/component/cas.class.php', 
-        'Activator' => '/package/activate/activator.class.php', 
-        'Deactivator' => '/package/deactivate/deactivator.class.php', 
-        'Installer' => '/package/install/installer.class.php');
+     private static $map = array(
+         'Autoloader' => '/autoloader.class.php',
+         'DataManager' => '/lib/data_manager.class.php',
+         'DataManagerInterface' => '/lib/data_manager_interface.class.php',
+         'DoctrineDataManager' => '/lib/data_manager/doctrine.class.php',
+         'Mdb2DataManager' => '/lib/data_manager/mdb2.class.php',
+         'Manager' => '/lib/manager/manager.class.php',
+         'AtlantisComponent' => '/lib/manager/component/atlantis.class.php',
+         'BamaflexComponent' => '/lib/manager/component/bamaflex.class.php',
+         'BrowserComponent' => '/lib/manager/component/browser.class.php',
+         'CasComponent' => '/lib/manager/component/cas.class.php',
+         'Activator' => '/package/activate/activator.class.php',
+         'Deactivator' => '/package/deactivate/deactivator.class.php',
+         'Installer' => '/package/install/installer.class.php'
+    );
 
     /**
      * Try to load the class
-     * 
+     *
      * @param $classname string
      * @return boolean
      */
@@ -37,13 +38,13 @@ class Autoloader
             require_once __DIR__ . self :: $map[$classname];
             return true;
         }
-        
+
         return false;
     }
 
     /**
      * Synchronize the autoloader
-     * 
+     *
      * @param $update boolean
      * @return multitype:string
      */
@@ -51,4 +52,6 @@ class Autoloader
     {
         return \common\libraries\AutoloaderUtilities :: synch(__DIR__, __DIR__, $update);
     }
+
 }
+?>
