f10f8ce674773a4522c4775feeef076e18c2b4ec
Hans De Bisschop <hans.de.bisschop@ehb.be>
2014-09-18 11:40:59 +0200

    Merge

--- Application/Atlantis/role/entity/php/ajax/contexts_feed.class.php
+++ Application/Atlantis/role/entity/php/ajax/contexts_feed.class.php
@@ -8,18 +8,18 @@ use libraries\OrCondition;
 use libraries\EqualityCondition;
 use libraries\AndCondition;
 use libraries\DataClassRetrievesParameters;
-use libraries\ObjectTableOrder;
+use libraries\StaticConditionVariable;
 use libraries\ArrayResultSet;
 use libraries\DataClassCountParameters;
 use libraries\NotCondition;
 use core\group\Group;
 use core\group\GroupAjaxPlatformGroupsFeed;
 use libraries\PropertyConditionVariable;
-use libraries\StaticConditionVariable;
+use libraries\OrderBy;
 
 /**
  * Feed to return the platform groups for the platform group entity
- * 
+ *
  * @package roup
  * @author Sven Vanpoucke
  */
@@ -32,50 +32,50 @@ class EntityAjaxContextsFeed extends GroupAjaxPlatformGroupsFeed
 
     /**
      * Returns the element for a specific group
-     * 
+     *
      * @return AdvancedElementFinderElement
      */
     public function get_group_element($group)
     {
         $code_conditions = array();
         $code_conditions[] = new PatternMatchCondition(
-            new PropertyConditionVariable(Group :: class_name(), Group :: PROPERTY_CODE), 
+            new PropertyConditionVariable(Group :: class_name(), Group :: PROPERTY_CODE),
             'AY_*');
         $code_conditions[] = new PatternMatchCondition(
-            new PropertyConditionVariable(Group :: class_name(), Group :: PROPERTY_CODE), 
+            new PropertyConditionVariable(Group :: class_name(), Group :: PROPERTY_CODE),
             'CA*');
         $code_conditions[] = new PatternMatchCondition(
-            new PropertyConditionVariable(Group :: class_name(), Group :: PROPERTY_CODE), 
+            new PropertyConditionVariable(Group :: class_name(), Group :: PROPERTY_CODE),
             'DEP_*');
         $code_conditions[] = new PatternMatchCondition(
-            new PropertyConditionVariable(Group :: class_name(), Group :: PROPERTY_CODE), 
+            new PropertyConditionVariable(Group :: class_name(), Group :: PROPERTY_CODE),
             'TRA_OP_*');
         $code_conditions[] = new PatternMatchCondition(
-            new PropertyConditionVariable(Group :: class_name(), Group :: PROPERTY_CODE), 
+            new PropertyConditionVariable(Group :: class_name(), Group :: PROPERTY_CODE),
             'TRA_STU_*');
-        
+
         $not_code_conditions = array();
         $not_code_conditions[] = new NotCondition(
             new PatternMatchCondition(
-                new PropertyConditionVariable(Group :: class_name(), Group :: PROPERTY_CODE), 
+                new PropertyConditionVariable(Group :: class_name(), Group :: PROPERTY_CODE),
                 'TRA_STU_*_*'));
         $not_code_conditions[] = new NotCondition(
             new PatternMatchCondition(
-                new PropertyConditionVariable(Group :: class_name(), Group :: PROPERTY_CODE), 
+                new PropertyConditionVariable(Group :: class_name(), Group :: PROPERTY_CODE),
                 'COU_OP_*'));
-        
+
         $conditions = array();
         $conditions[] = new OrCondition($code_conditions);
         $conditions[] = new AndCondition($not_code_conditions);
         $conditions[] = new EqualityCondition(
-            new PropertyConditionVariable(Group :: class_name(), Group :: PROPERTY_PARENT_ID), 
+            new PropertyConditionVariable(Group :: class_name(), Group :: PROPERTY_PARENT_ID),
             new StaticConditionVariable($group->get_id()));
         $condition = new AndCondition($conditions);
-        
+
         $children_count = \core\group\DataManager :: count(
-            Group :: class_name(), 
+            Group :: class_name(),
             new DataClassCountParameters($condition));
-        
+
         if ($this->get_user()->is_platform_admin())
         {
             if ($children_count > 0)
@@ -92,7 +92,7 @@ class EntityAjaxContextsFeed extends GroupAjaxPlatformGroupsFeed
         elseif (\application\atlantis\rights\Rights :: get_instance()->access_is_allowed())
         {
             $target_groups = \application\atlantis\rights\Rights :: get_instance()->get_target_groups($this->get_user());
-            
+
             if (in_array($group->get_id(), $target_groups))
             {
                 if ($children_count > 0)
@@ -125,7 +125,7 @@ class EntityAjaxContextsFeed extends GroupAjaxPlatformGroupsFeed
                         break;
                     }
                 }
-                
+
                 // if (! $type)
                 // {
                 // $element_type = AdvancedElementFinderElement :: TYPE_FILTER;
@@ -138,32 +138,32 @@ class EntityAjaxContextsFeed extends GroupAjaxPlatformGroupsFeed
             $element_type = AdvancedElementFinderElement :: TYPE_FILTER;
             $element_class = 'type type_context_folder';
         }
-        
+
         return new AdvancedElementFinderElement(
-            PlatformGroupEntity :: ENTITY_TYPE . '_' . $group->get_id(), 
-            $element_class, 
-            $group->get_name(), 
-            $group->get_code(), 
+            PlatformGroupEntity :: ENTITY_TYPE . '_' . $group->get_id(),
+            $element_class,
+            $group->get_name(),
+            $group->get_code(),
             $element_type);
     }
 
     /**
      * Returns the element for a specific user
-     * 
+     *
      * @return AdvancedElementFinderElement
      */
     public function get_user_element($user)
     {
         return new AdvancedElementFinderElement(
-            UserEntity :: ENTITY_TYPE . '_' . $user->get_id(), 
-            'type type_user', 
-            $user->get_fullname(), 
+            UserEntity :: ENTITY_TYPE . '_' . $user->get_id(),
+            'type type_user',
+            $user->get_fullname(),
             $user->get_official_code());
     }
 
     /**
      * Returns all the groups for this feed
-     * 
+     *
      * @return ResultSet
      */
     public function retrieve_groups()
@@ -174,67 +174,71 @@ class EntityAjaxContextsFeed extends GroupAjaxPlatformGroupsFeed
         {
             $q = '*' . $search_query . '*';
             $name_conditions[] = new PatternMatchCondition(
-                new PropertyConditionVariable(Group :: class_name(), Group :: PROPERTY_NAME), 
+                new PropertyConditionVariable(Group :: class_name(), Group :: PROPERTY_NAME),
                 $q);
             $name_conditions[] = new PatternMatchCondition(
-                new PropertyConditionVariable(Group :: class_name(), Group :: PROPERTY_CODE), 
+                new PropertyConditionVariable(Group :: class_name(), Group :: PROPERTY_CODE),
                 $q);
             $conditions[] = new OrCondition($name_conditions);
         }
-        
+
         $code_conditions = array();
         $code_conditions[] = new PatternMatchCondition(
-            new PropertyConditionVariable(Group :: class_name(), Group :: PROPERTY_CODE), 
+            new PropertyConditionVariable(Group :: class_name(), Group :: PROPERTY_CODE),
             'AY_*');
         $code_conditions[] = new PatternMatchCondition(
-            new PropertyConditionVariable(Group :: class_name(), Group :: PROPERTY_CODE), 
+            new PropertyConditionVariable(Group :: class_name(), Group :: PROPERTY_CODE),
             'CA*');
         $code_conditions[] = new PatternMatchCondition(
-            new PropertyConditionVariable(Group :: class_name(), Group :: PROPERTY_CODE), 
+            new PropertyConditionVariable(Group :: class_name(), Group :: PROPERTY_CODE),
             'DEP_*');
         $code_conditions[] = new PatternMatchCondition(
-            new PropertyConditionVariable(Group :: class_name(), Group :: PROPERTY_CODE), 
+            new PropertyConditionVariable(Group :: class_name(), Group :: PROPERTY_CODE),
             'TRA_OP_*');
         $code_conditions[] = new PatternMatchCondition(
-            new PropertyConditionVariable(Group :: class_name(), Group :: PROPERTY_CODE), 
+            new PropertyConditionVariable(Group :: class_name(), Group :: PROPERTY_CODE),
             'TRA_STU_*');
         $code_conditions[] = new EqualityCondition(
-            new PropertyConditionVariable(Group :: class_name(), Group :: PROPERTY_PARENT_ID), 
+            new PropertyConditionVariable(Group :: class_name(), Group :: PROPERTY_PARENT_ID),
             new StaticConditionVariable(0));
 
         $conditions[] = new OrCondition($code_conditions);
-        
+
         $filter_id = $this->get_filter();
-        
+
         if ($filter_id)
         {
             $conditions[] = new EqualityCondition(
-                new PropertyConditionVariable(Group :: class_name(), Group :: PROPERTY_PARENT_ID), 
+                new PropertyConditionVariable(Group :: class_name(), Group :: PROPERTY_PARENT_ID),
                 new StaticConditionVariable($filter_id));
         }
         else
         {
             $conditions[] = new EqualityCondition(
-                new PropertyConditionVariable(Group :: class_name(), Group :: PROPERTY_PARENT_ID), 
+                new PropertyConditionVariable(Group :: class_name(), Group :: PROPERTY_PARENT_ID),
                 new StaticConditionVariable(0));
         }
-        
+
         // Combine the conditions
         $count = count($conditions);
         if ($count > 1)
         {
             $condition = new AndCondition($conditions);
         }
-        
+
         if ($count == 1)
         {
             $condition = $conditions[0];
         }
-        
+
         $groups = \core\group\DataManager :: retrieves(
-            Group :: class_name(), 
-            new DataClassRetrievesParameters($condition, null, null, array(new ObjectTableOrder(Group :: PROPERTY_NAME))));
-        
+            Group :: class_name(),
+            new DataClassRetrievesParameters(
+                $condition,
+                null,
+                null,
+                array(new OrderBy(new PropertyConditionVariable(Group :: class_name(), Group :: PROPERTY_NAME)))));
+
         if ($this->get_user()->is_platform_admin())
         {
             return $groups;
@@ -242,16 +246,16 @@ class EntityAjaxContextsFeed extends GroupAjaxPlatformGroupsFeed
         elseif (\application\atlantis\rights\Rights :: get_instance()->access_is_allowed())
         {
             $target_groups = \application\atlantis\rights\Rights :: get_instance()->get_target_groups($this->get_user());
-            
+
             $allowed_groups = array();
-            
+
             while ($group = $groups->next_result())
             {
                 foreach ($target_groups as $target_group)
                 {
                     $is_parent = $group->is_parent_of($target_group);
                     $is_child = $group->is_child_of($target_group);
-                    
+
                     if ($is_parent || $is_child || $target_group == $group->get_id())
                     {
                         $allowed_groups[] = $group;
@@ -259,7 +263,7 @@ class EntityAjaxContextsFeed extends GroupAjaxPlatformGroupsFeed
                     }
                 }
             }
-            
+
             return new ArrayResultSet($allowed_groups);
         }
     }
--- Application/Atlantis/role/entity/php/ajax/user_entity_feed.class.php
+++ Application/Atlantis/role/entity/php/ajax/user_entity_feed.class.php
@@ -6,17 +6,17 @@ use libraries\Request;
 use libraries\Utilities;
 use libraries\AndCondition;
 use libraries\DataClassRetrievesParameters;
-use libraries\ObjectTableOrder;
+use libraries\StaticConditionVariable;
 use libraries\InCondition;
 use libraries\EqualityCondition;
 use libraries\DataClassCountParameters;
 use core\user\User;
 use libraries\PropertyConditionVariable;
-use libraries\StaticConditionVariable;
+use libraries\OrderBy;
 
 /**
  * Feed to return users from the user entity
- * 
+ *
  * @package rights
  * @author Sven Vanpoucke - Hogeschool Gent
  */
@@ -25,7 +25,7 @@ class EntityAjaxUserEntityFeed extends \core\user\UserAjaxUsersFeed
 
     /**
      * Returns the advanced element finder element for the given user
-     * 
+     *
      * @param User $user
      *
      * @return AdvancedElementFinderElement
@@ -33,68 +33,70 @@ class EntityAjaxUserEntityFeed extends \core\user\UserAjaxUsersFeed
     protected function get_element_for_user($user)
     {
         return new AdvancedElementFinderElement(
-            UserEntity :: ENTITY_TYPE . '_' . $user->get_id(), 
-            'type type_user', 
-            $user->get_fullname(), 
+            UserEntity :: ENTITY_TYPE . '_' . $user->get_id(),
+            'type type_user',
+            $user->get_fullname(),
             $user->get_official_code());
     }
 
     /**
      * Retrieves the users from the course (direct subscribed and group subscribed)
-     * 
+     *
      * @return ResultSet
      */
     public function retrieve_users()
     {
         $search_query = Request :: post(self :: PARAM_SEARCH_QUERY);
-        
+
         // Set the conditions for the search query
         if ($search_query && $search_query != '')
         {
             $conditions[] = Utilities :: query_to_condition(
-                $search_query, 
+                $search_query,
                 array(User :: PROPERTY_USERNAME, User :: PROPERTY_FIRSTNAME, User :: PROPERTY_LASTNAME));
         }
-        
+
         if (! $this->get_user()->is_platform_admin() &&
              \application\atlantis\rights\Rights :: get_instance()->access_is_allowed())
         {
             $target_users = \application\atlantis\rights\Rights :: get_instance()->get_target_users($this->get_user());
-            
+
             if (count($target_users) > 0)
             {
                 $conditions[] = new InCondition(
-                    new PropertyConditionVariable(User :: class_name(), User :: PROPERTY_ID), 
+                    new PropertyConditionVariable(User :: class_name(), User :: PROPERTY_ID),
                     $target_users);
             }
             else
             {
                 $conditions[] = new EqualityCondition(
-                    new PropertyConditionVariable(User :: class_name(), User :: PROPERTY_ID), 
+                    new PropertyConditionVariable(User :: class_name(), User :: PROPERTY_ID),
                     new StaticConditionVariable(- 1));
             }
         }
-        
+
         // Combine the conditions
         $count = count($conditions);
         if ($count > 1)
         {
             $condition = new AndCondition($conditions);
         }
-        
+
         if ($count == 1)
         {
             $condition = $conditions[0];
         }
-        
+
         $this->set_user_count(
             \core\user\DataManager :: count(User :: class_name(), new DataClassCountParameters($condition)));
         $parameters = new DataClassRetrievesParameters(
-            $condition, 
-            100, 
-            $this->get_offset(), 
-            array(new ObjectTableOrder(User :: PROPERTY_FIRSTNAME), new ObjectTableOrder(User :: PROPERTY_LASTNAME)));
-        
+            $condition,
+            100,
+            $this->get_offset(),
+            array(
+                new OrderBy(new PropertyConditionVariable(User :: class_name(), User :: PROPERTY_FIRSTNAME)),
+                new OrderBy(new PropertyConditionVariable(User :: class_name(), User :: PROPERTY_LASTNAME))));
+
         return \core\user\DataManager :: retrieves(User :: class_name(), $parameters);
     }
 }
