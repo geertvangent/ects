b78d3c3e0f9ff8d154b2d92c80ecb9bd2258986b
Hans De Bisschop <hans.de.bisschop@ehb.be>
2015-01-23 17:11:34 +0100

    Fix: Removed dead components

--- Application/Atlantis/Application/Component/RightsComponent.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace Ehb\Application\Atlantis\Application\Component;
-
-use Ehb\Application\Atlantis\Application\Manager;
-
-class RightsComponent extends Manager
-{
-}
--- Application/Atlantis/Role/Component/RightsComponent.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace Ehb\Application\Atlantis\Role\Component;
-
-use Ehb\Application\Atlantis\Role\Manager;
-
-class RightsComponent extends Manager
-{
-}
