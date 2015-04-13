88ca33a79ca01322cf552345677712c50319623b
Hans De Bisschop <hans.de.bisschop@ehb.be>
2015-04-13 17:14:49 +0200

    Feature: Metadata moved to public repository

--- Core/Metadata/Integration/Chamilo/Core/Admin/Package/Activator.php
+++
@@ -1,6 +0,0 @@
-<?php
-namespace Ehb\Core\Metadata\Integration\Chamilo\Core\Admin\Package;
-
-class Activator extends \Chamilo\Configuration\Package\Action\Activator
-{
-}
--- Core/Metadata/Integration/Chamilo/Core/Admin/Package/Installer.php
+++
@@ -1,6 +0,0 @@
-<?php
-namespace Ehb\Core\Metadata\Integration\Chamilo\Core\Admin\Package;
-
-class Installer extends \Chamilo\Configuration\Package\Action\Installer
-{
-}
