f4132182eb1ea0732cc24f8e0f2d1e13ac436e2e
Hans De Bisschop <hans.de.bisschop@ehb.be>
2015-12-08 13:29:44 +0100

    Fix: Make sure the normal branch still exists

--- Application/Helpdesk/Manager.php
+++ Application/Helpdesk/Manager.php
@@ -3,6 +3,13 @@ namespace Ehb\Application\Helpdesk;
 
 use Chamilo\Libraries\Architecture\Application\Application;
 
+/**
+ *
+ * @package Ehb\Application\Helpdesk
+ * @author Hans De Bisschop <hans.de.bisschop@ehb.be>
+ * @author Magali Gillard <magali.gillard@ehb.be>
+ * @author Eduard Vossen <eduard.vossen@ehb.be>
+ */
 abstract class Manager extends Application
 {
     const ACTION_CREATE = 'Creator';
