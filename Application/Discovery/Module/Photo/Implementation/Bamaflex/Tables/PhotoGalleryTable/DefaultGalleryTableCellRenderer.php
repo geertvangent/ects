c30b48a9cb90f0ca615af40c1167a53a79e39c37
Hans De Bisschop <hans.de.bisschop@ehb.be>
2016-07-05 08:32:46 +0200

    Bugfix

--- Application/Discovery/Module/Photo/Implementation/Bamaflex/Tables/PhotoGalleryTable/DefaultGalleryTableCellRenderer.php
+++ Application/Discovery/Module/Photo/Implementation/Bamaflex/Tables/PhotoGalleryTable/DefaultGalleryTableCellRenderer.php
@@ -6,13 +6,6 @@ use Chamilo\Libraries\Format\Table\Extension\GalleryTable\GalleryTableCellRender
 abstract class DefaultGalleryTableCellRenderer extends GalleryTableCellRenderer
 {
 
-    public function render_cell($user)
-    {
-        $html = array();
-        $html[] = $this->renderContent($user);
-        return implode(PHP_EOL, $html);
-    }
-
     public function render_id_cell($user)
     {
         return $user->get_id();
