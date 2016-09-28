c96262ff1828794c337198bbfa1f7050fcf7af3d
Hans De Bisschop <hans.de.bisschop@ehb.be>
2016-09-28 12:45:50 +0200

    Added a BC component to avoid errors

---
+++ Application/Calendar/Extension/SyllabusPlus/Component/BrowserComponent.php
@@ -0,0 +1,13 @@
+<?php
+namespace Ehb\Application\Calendar\Extension\SyllabusPlus\Component;
+
+/**
+ *
+ * @package Ehb\Application\Calendar\Extension\SyllabusPlus\Component
+ * @author Hans De Bisschop <hans.de.bisschop@ehb.be>
+ * @author Magali Gillard <magali.gillard@ehb.be>
+ * @deprecated Only used to make older links work
+ */
+class BrowserComponent extends UserBrowserComponent
+{
+}
