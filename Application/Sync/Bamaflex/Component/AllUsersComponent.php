666586071f8b9ccc04da25fb3d33a91508931857
ScaraMcDuck <hans.de.bisschop@ehb.be>
2017-01-04 10:43:23 +0100

    [Sync] Fixes for the database connection

--- Application/Sync/Bamaflex/Component/AllUsersComponent.php
+++ Application/Sync/Bamaflex/Component/AllUsersComponent.php
@@ -16,16 +16,16 @@ class AllUsersComponent extends Manager implements DelegateComponent
         ini_set("memory_limit", "-1");
         ini_set("max_execution_time", "18000");
         header('Content-Type: text/html; charset=utf-8');
-        
+
         try
         {
             echo '<pre>';
             echo '[USER SYNC STARTED] ' . date('c', time()) . "\n";
             flush();
-            
+
             $synchronization = UserSynchronization::factory('all');
             $synchronization->run();
-            
+
             echo '[  USER SYNC ENDED] ' . date('c', time()) . "\n";
             echo '</pre>';
         }
--- Application/Sync/Bamaflex/DataConnector/Bamaflex/BamaflexConnection.php
+++ Application/Sync/Bamaflex/DataConnector/Bamaflex/BamaflexConnection.php
@@ -7,7 +7,7 @@ use Doctrine\DBAL\DriverManager;
 
 /**
  * This class represents the current CAS Account database connection.
- * 
+ *
  * @author Hans De Bisschop
  */
 class BamaflexConnection extends \Chamilo\Libraries\Storage\DataManager\Doctrine\Connection
@@ -33,21 +33,24 @@ class BamaflexConnection extends \Chamilo\Libraries\Storage\DataManager\Doctrine
         $password = Configuration::getInstance()->get_setting(array('Ehb\Application\Sync', 'password'));
         $host = Configuration::getInstance()->get_setting(array('Ehb\Application\Sync', 'host'));
         $database = Configuration::getInstance()->get_setting(array('Ehb\Application\Sync', 'database'));
-        
-        $data_source_name = DataSourceName::factory('doctrine', $dbms, $user, $host, $database, $password);
+
+        $data_source_name = DataSourceName::factory(
+            'Doctrine',
+            array('driver' => $dbms, 'username' => $user, 'host' => $host, 'name' => $database, 'password' => $password));
+
         $configuration = new \Doctrine\DBAL\Configuration();
         $connection_parameters = array(
-            'dbname' => $data_source_name->get_database(), 
-            'user' => $data_source_name->get_username(), 
-            'password' => $data_source_name->get_password(), 
-            'host' => $data_source_name->get_host(), 
+            'dbname' => $data_source_name->get_database(),
+            'user' => $data_source_name->get_username(),
+            'password' => $data_source_name->get_password(),
+            'host' => $data_source_name->get_host(),
             'driverClass' => $data_source_name->get_driver(true));
         $this->connection = DriverManager::getConnection($connection_parameters, $configuration);
     }
 
     /**
      * Returns the instance of this class.
-     * 
+     *
      * @return Connection The instance.
      */
     public static function getInstance()
@@ -61,7 +64,7 @@ class BamaflexConnection extends \Chamilo\Libraries\Storage\DataManager\Doctrine
 
     /**
      * Gets the database connection.
-     * 
+     *
      * @return mixed MDB2 DB Conenction.
      */
     public function get_connection()
